fe4d71ddc891359452646fbfb49ba870c07283a3
Omega <emomega@proton.me>
2026-09-13 22:03:19 -0400

    Update mod.rs

--- engines/mod.rs
+++ engines/mod.rs
@@ -1,7 +1,27 @@
 // Nexus Browser Engine System
 // GPL-3.0 License
 
+pub mod engine_tester;
+pub mod engine_sandbox;
+pub mod engine_health;
+pub mod engine_trust;
+pub mod engine_hot_reload;
+pub mod engine_rollback;
+pub mod source_watcher;
+pub mod artifact_manager;
 
+
+pub use engine_tester::EngineTester;
+pub use engine_sandbox::EngineSandbox;
+pub use engine_health::EngineHealth;
+pub use engine_trust::EngineTrust;
+pub use engine_hot_reload::EngineHotReload;
+pub use engine_rollback::EngineRollback;
+pub use source_watcher::SourceWatcher;
+pub use artifact_manager::{
+    ArtifactManager,
+    EngineArtifact,
+};
 pub mod engine_api;
 pub mod engine_manager;
 pub mod engine_manifest;
