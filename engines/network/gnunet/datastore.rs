0278b43811fb29d58625e4ca10de0731a76cd217
Omega <emomega@proton.me>
2026-09-14 23:33:12 -0400

    Implement DataStore struct with basic methods

---
+++ engines/network/gnunet/datastore.rs
@@ -0,0 +1,84 @@
+//! GNUnet distributed datastore.
+
+use std::collections::HashMap;
+
+
+
+pub struct DataStore {
+
+
+    entries:
+        HashMap<String,Vec<u8>>,
+
+
+}
+
+
+
+impl DataStore {
+
+
+    pub fn new() -> Self {
+
+
+        Self {
+
+            entries:
+                HashMap::new(),
+
+        }
+
+    }
+
+
+
+    pub fn store(
+        &mut self,
+        key:String,
+        data:Vec<u8>
+    ) {
+
+
+        self.entries
+            .insert(
+                key,
+                data
+            );
+
+    }
+
+
+
+    pub fn retrieve(
+        &self,
+        key:&str
+    ) -> Option<&Vec<u8>> {
+
+
+        self.entries.get(key)
+
+    }
+
+
+
+    pub fn count(
+        &self
+    ) -> usize {
+
+        self.entries.len()
+
+    }
+
+}
+
+
+
+impl Default for DataStore {
+
+    fn default() -> Self {
+
+        Self::new()
+
+    }
+
+}
