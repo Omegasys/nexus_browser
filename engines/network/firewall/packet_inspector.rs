a972314a62970cc31ff787141458db90a63ab9c1
Omega <emomega@proton.me>
2026-09-15 00:13:04 -0400

    Create packet_inspector.rs

---
+++ engines/network/firewall/packet_inspector.rs
@@ -0,0 +1,20 @@
+//! Packet inspection layer.
+
+
+pub struct PacketInspector;
+
+
+
+impl PacketInspector {
+
+
+    pub fn inspect(
+        &self,
+        _packet:&str
+    ) -> bool {
+
+        true
+
+    }
+
+}
