4abd970f3d52fb57220ac554fb6775c175fde411
Omega <emomega@proton.me>
2026-09-21 23:27:36 -0400

    Create mod.rs

---
+++ tests/networking/routing/mod.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod route_selection;
+
+#[cfg(test)]
+mod per_tab_routing;
+
+#[cfg(test)]
+mod per_workspace_routing;
+
+#[cfg(test)]
+mod per_profile_routing;
+
+#[cfg(test)]
+mod route_isolation;
+
+#[cfg(test)]
+mod fallback_policy;
