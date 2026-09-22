4cdd43d211ee2b4f566d84953797065dda8af1a1
Omega <emomega@proton.me>
2026-09-21 23:31:38 -0400

    Add test modules for DNS functionality

---
+++ tests/dns/mod.rs
@@ -0,0 +1,47 @@
+#[cfg(test)]
+mod dns_manager;
+
+#[cfg(test)]
+mod secure_dns;
+
+#[cfg(test)]
+mod doh;
+
+#[cfg(test)]
+mod dot;
+
+#[cfg(test)]
+mod dnscrypt;
+
+#[cfg(test)]
+mod dnssec;
+
+#[cfg(test)]
+mod resolver_registry;
+
+#[cfg(test)]
+mod resolver_health;
+
+#[cfg(test)]
+mod dns_cache;
+
+#[cfg(test)]
+mod cache_partitioning;
+
+#[cfg(test)]
+mod dns_policy;
+
+#[cfg(test)]
+mod dns_kill_switch;
+
+#[cfg(test)]
+mod resolver_selection;
+
+#[cfg(test)]
+mod dns_failover;
+
+#[cfg(test)]
+mod dns_leak_prevention;
+
+#[cfg(test)]
+mod dns_isolation;
