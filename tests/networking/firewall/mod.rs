9830b3ec0d4f7b1ebeae13647abd4cb3f4f0fe42
Omega <emomega@proton.me>
2026-09-21 23:18:35 -0400

    Add firewall module tests

---
+++ tests/networking/firewall/mod.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod firewall_rules;
+
+#[cfg(test)]
+mod packet_filtering;
+
+#[cfg(test)]
+mod ip_filtering;
+
+#[cfg(test)]
+mod port_filtering;
+
+#[cfg(test)]
+mod domain_filtering;
+
+#[cfg(test)]
+mod firewall_kill_switch;
