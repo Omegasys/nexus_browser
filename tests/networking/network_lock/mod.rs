2c6d6369e2fbba275557c90a622fbc1bb80e2a48
Omega <emomega@proton.me>
2026-09-21 23:25:32 -0400

    Add test modules for networking locks

---
+++ tests/networking/network_lock/mod.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod all_traffic;
+
+#[cfg(test)]
+mod dns_lock;
+
+#[cfg(test)]
+mod ipv4_lock;
+
+#[cfg(test)]
+mod ipv6_lock;
+
+#[cfg(test)]
+mod tcp_lock;
+
+#[cfg(test)]
+mod udp_lock;
+
+#[cfg(test)]
+mod quic_lock;
+
+#[cfg(test)]
+mod bypass_prevention;
