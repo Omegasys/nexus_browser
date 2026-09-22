97c39b2e60f5ee7b95adf467155665c80cf647a5
Omega <emomega@proton.me>
2026-09-21 22:29:16 -0400

    Add test modules for engine components

---
+++ tests/engines/mod.rs
@@ -0,0 +1,50 @@
+#[cfg(test)]
+mod engine_api;
+
+#[cfg(test)]
+mod engine_manager;
+
+#[cfg(test)]
+mod engine_manifest;
+
+#[cfg(test)]
+mod engine_compiler;
+
+#[cfg(test)]
+mod engine_builder;
+
+#[cfg(test)]
+mod engine_validator;
+
+#[cfg(test)]
+mod engine_tester;
+
+#[cfg(test)]
+mod engine_sandbox;
+
+#[cfg(test)]
+mod engine_health;
+
+#[cfg(test)]
+mod engine_trust;
+
+#[cfg(test)]
+mod engine_hot_reload;
+
+#[cfg(test)]
+mod engine_rollback;
+
+#[cfg(test)]
+mod source_watcher;
+
+#[cfg(test)]
+mod artifact_manager;
+
+#[cfg(test)]
+mod rendering_engines;
+
+#[cfg(test)]
+mod engine_capabilities;
+
+#[cfg(test)]
+mod engine_isolation;
