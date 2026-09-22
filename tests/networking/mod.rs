c6a970c68e9a3f356442270f6b9880333509f50f
Omega <emomega@proton.me>
2026-09-21 23:07:22 -0400

    Add test modules for networking functionality

---
+++ tests/networking/mod.rs
@@ -0,0 +1,44 @@
+#[cfg(test)]
+mod direct;
+
+#[cfg(test)]
+mod proxy;
+
+#[cfg(test)]
+mod vpn;
+
+#[cfg(test)]
+mod tor;
+
+#[cfg(test)]
+mod i2p;
+
+#[cfg(test)]
+mod nym;
+
+#[cfg(test)]
+mod lokinet;
+
+#[cfg(test)]
+mod dns;
+
+#[cfg(test)]
+mod firewall;
+
+#[cfg(test)]
+mod protocol_filter;
+
+#[cfg(test)]
+mod connection;
+
+#[cfg(test)]
+mod leaks;
+
+#[cfg(test)]
+mod network_lock;
+
+#[cfg(test)]
+mod routing;
+
+#[cfg(test)]
+mod network_policy;
