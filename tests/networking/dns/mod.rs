44dad449c00fef528fc18a91d2ce838b046c8014
Omega <emomega@proton.me>
2026-09-21 23:16:44 -0400

    Add dns module with test configurations

---
+++ tests/networking/dns/mod.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod dns_resolution;
+
+#[cfg(test)]
+mod doh;
+
+#[cfg(test)]
+mod dot;
+
+#[cfg(test)]
+mod dnscrypt;
+
+#[cfg(test)]
+mod dnssec;
+
+#[cfg(test)]
+mod dns_cache;
+
+#[cfg(test)]
+mod dns_partitioning;
+
+#[cfg(test)]
+mod dns_fail_closed;
