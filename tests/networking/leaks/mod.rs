bfa6374a08146f8752cde1db2047856609267ced
Omega <emomega@proton.me>
2026-09-21 23:23:20 -0400

    Add test modules for networking leak tests

---
+++ tests/networking/leaks/mod.rs
@@ -0,0 +1,26 @@
+#[cfg(test)]
+mod dns_leak;
+
+#[cfg(test)]
+mod ipv4_leak;
+
+#[cfg(test)]
+mod ipv6_leak;
+
+#[cfg(test)]
+mod webrtc_leak;
+
+#[cfg(test)]
+mod direct_connection_leak;
+
+#[cfg(test)]
+mod proxy_bypass;
+
+#[cfg(test)]
+mod vpn_bypass;
+
+#[cfg(test)]
+mod tor_bypass;
+
+#[cfg(test)]
+mod interface_exposure;
