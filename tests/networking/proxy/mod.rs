b6b2316945e58bb90b76f39245640e7787c5b441
Omega <emomega@proton.me>
2026-09-21 23:09:11 -0400

    Add test modules for various proxy types

---
+++ tests/networking/proxy/mod.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod http_proxy;
+
+#[cfg(test)]
+mod https_proxy;
+
+#[cfg(test)]
+mod socks4;
+
+#[cfg(test)]
+mod socks5;
+
+#[cfg(test)]
+mod proxy_chain;
+
+#[cfg(test)]
+mod proxy_failover;
+
+#[cfg(test)]
+mod proxy_leak_prevention;
