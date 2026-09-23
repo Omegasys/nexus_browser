774fdfed4a288764699f793c9db3c490131c2826
Omega <emomega@proton.me>
2026-09-22 22:56:13 -0400

    Add privacy test modules

---
+++ tests/privacy/mod.rs
@@ -0,0 +1,32 @@
+#[cfg(test)]
+mod privacy_manager;
+
+#[cfg(test)]
+mod privacy_profiles;
+
+#[cfg(test)]
+mod privacy_policy;
+
+#[cfg(test)]
+mod privacy_score;
+
+#[cfg(test)]
+mod fingerprinting;
+
+#[cfg(test)]
+mod tracking;
+
+#[cfg(test)]
+mod webrtc;
+
+#[cfg(test)]
+mod storage;
+
+#[cfg(test)]
+mod identity;
+
+#[cfg(test)]
+mod network;
+
+#[cfg(test)]
+mod privacy_regression;
