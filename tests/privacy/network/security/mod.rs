88806544384004060bdd85b7975c58e6c19f356c
Omega <emomega@proton.me>
2026-09-22 23:12:59 -0400

    Add security module tests in mod.rs

---
+++ tests/privacy/network/security/mod.rs
@@ -0,0 +1,26 @@
+#[cfg(test)]
+mod security_manager;
+
+#[cfg(test)]
+mod sandboxing;
+
+#[cfg(test)]
+mod certificates;
+
+#[cfg(test)]
+mod permissions;
+
+#[cfg(test)]
+mod content_security;
+
+#[cfg(test)]
+mod extensions;
+
+#[cfg(test)]
+mod downloads;
+
+#[cfg(test)]
+mod network;
+
+#[cfg(test)]
+mod security_regression;
