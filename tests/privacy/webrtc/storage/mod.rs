941546dba4c08fa634f616b969d24f09ae807659
Omega <emomega@proton.me>
2026-09-22 23:04:52 -0400

    Create mod.rs

---
+++ tests/privacy/webrtc/storage/mod.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod storage_partitioning;
+
+#[cfg(test)]
+mod cookie_isolation;
+
+#[cfg(test)]
+mod cache_isolation;
+
+#[cfg(test)]
+mod indexeddb_isolation;
+
+#[cfg(test)]
+mod local_storage_isolation;
+
+#[cfg(test)]
+mod session_storage_isolation;
+
+#[cfg(test)]
+mod site_data_deletion;
