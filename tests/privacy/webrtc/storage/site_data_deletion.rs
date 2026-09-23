e46c81f9374b575c17da8b5becda0d105a806946
Omega <emomega@proton.me>
2026-09-22 23:06:06 -0400

    Create site_data_deletion.rs

---
+++ tests/privacy/webrtc/storage/site_data_deletion.rs
@@ -0,0 +1,13 @@
+#[test]
+fn site_data_can_be_deleted() {
+    let deleted = true;
+
+    assert!(deleted);
+}
+
+#[test]
+fn all_site_data_can_be_deleted() {
+    let deleted = true;
+
+    assert!(deleted);
+}
