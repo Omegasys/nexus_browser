c6574be084a8b87a15368161f9fa5912edbf8196
Omega <emomega@proton.me>
2026-09-22 22:57:28 -0400

    Create fingerprint_protection.rs

---
+++ tests/privacy/fingerprinting/fingerprint_protection.rs
@@ -0,0 +1,13 @@
+#[test]
+fn fingerprint_protection_can_be_enabled() {
+    let enabled = true;
+
+    assert!(enabled);
+}
+
+#[test]
+fn maximum_fingerprint_protection_can_randomize_signals() {
+    let randomized = true;
+
+    assert!(randomized);
+}
