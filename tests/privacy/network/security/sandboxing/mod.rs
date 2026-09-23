dda3218365d922c1e2b7b2494c67ef57818d8f22
Omega <emomega@proton.me>
2026-09-22 23:14:02 -0400

    Add sandboxing module with isolation tests

---
+++ tests/privacy/network/security/sandboxing/mod.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod process_isolation;
+
+#[cfg(test)]
+mod site_isolation;
+
+#[cfg(test)]
+mod tab_isolation;
+
+#[cfg(test)]
+mod capability_model;
+
+#[cfg(test)]
+mod vm_isolation;
+
+#[cfg(test)]
+mod escape_prevention;
