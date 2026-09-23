3c7442dce465fc4da2c084d0f94e134b7b406f0e
Omega <emomega@proton.me>
2026-09-22 22:57:16 -0400

    Add fingerprinting test modules

---
+++ tests/privacy/fingerprinting/mod.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod fingerprint_protection;
+
+#[cfg(test)]
+mod fingerprint_entropy;
+
+#[cfg(test)]
+mod canvas_protection;
+
+#[cfg(test)]
+mod webgl_protection;
+
+#[cfg(test)]
+mod audio_fingerprint;
+
+#[cfg(test)]
+mod font_fingerprint;
+
+#[cfg(test)]
+mod screen_fingerprint;
+
+#[cfg(test)]
+mod hardware_fingerprint;
