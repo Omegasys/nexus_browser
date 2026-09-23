261b7dc6366f00bd0b962a1636f5db567f8fadec
Omega <emomega@proton.me>
2026-09-22 22:59:06 -0400

    Add test modules for tracking features

---
+++ tests/privacy/tracking/mod.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tracker_protection;
+
+#[cfg(test)]
+mod third_party_tracking;
+
+#[cfg(test)]
+mod tracking_cookies;
+
+#[cfg(test)]
+mod link_tracking;
+
+#[cfg(test)]
+mod tracking_parameters;
+
+#[cfg(test)]
+mod telemetry;
