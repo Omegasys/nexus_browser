5706fc9b91a4ba0d55dbc7e51ea6c883d6e2d389
Omega <emomega@proton.me>
2026-09-24 22:49:35 -0400

    Add test modules for storage functionality

---
+++ tests/storage/mod.rs
@@ -0,0 +1,47 @@
+#[cfg(test)]
+mod cookie_manager;
+
+#[cfg(test)]
+mod cookie_policy;
+
+#[cfg(test)]
+mod cookie_deletion;
+
+#[cfg(test)]
+mod site_data_manager;
+
+#[cfg(test)]
+mod cache_manager;
+
+#[cfg(test)]
+mod indexeddb;
+
+#[cfg(test)]
+mod local_storage;
+
+#[cfg(test)]
+mod session_storage;
+
+#[cfg(test)]
+mod cache_storage;
+
+#[cfg(test)]
+mod service_workers;
+
+#[cfg(test)]
+mod shared_workers;
+
+#[cfg(test)]
+mod broadcast_channel;
+
+#[cfg(test)]
+mod storage_partitioning;
+
+#[cfg(test)]
+mod storage_isolation;
+
+#[cfg(test)]
+mod storage_expiration;
+
+#[cfg(test)]
+mod storage_regression;
