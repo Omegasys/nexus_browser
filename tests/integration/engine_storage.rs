32534c4b0b1ded6f6a21fd00808f24f27b77b934
Omega <emomega@proton.me>
2026-09-24 22:53:50 -0400

    Add test for partitioned access in engine storage

---
+++ tests/integration/engine_storage.rs
@@ -0,0 +1,8 @@
+#[test]
+fn rendering_engine_storage_access_can_be_partitioned() {
+    let engine = true;
+    let partitioned_storage = true;
+
+    assert!(engine);
+    assert!(partitioned_storage);
+}
