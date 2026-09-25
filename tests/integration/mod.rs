6df1d03a645e5a5fe6bb778d531b5531fee40fac
Omega <emomega@proton.me>
2026-09-24 22:52:51 -0400

    Add integration tests for various modules

---
+++ tests/integration/mod.rs
@@ -0,0 +1,86 @@
+#[cfg(test)]
+mod browser_startup;
+
+#[cfg(test)]
+mod browser_shutdown;
+
+#[cfg(test)]
+mod engine_networking;
+
+#[cfg(test)]
+mod engine_security;
+
+#[cfg(test)]
+mod engine_storage;
+
+#[cfg(test)]
+mod engine_microvm;
+
+#[cfg(test)]
+mod dns_networking;
+
+#[cfg(test)]
+mod dns_privacy;
+
+#[cfg(test)]
+mod dns_network_lock;
+
+#[cfg(test)]
+mod networking_privacy;
+
+#[cfg(test)]
+mod networking_security;
+
+#[cfg(test)]
+mod networking_storage;
+
+#[cfg(test)]
+mod privacy_storage;
+
+#[cfg(test)]
+mod privacy_security;
+
+#[cfg(test)]
+mod privacy_microvm;
+
+#[cfg(test)]
+mod security_microvm;
+
+#[cfg(test)]
+mod security_storage;
+
+#[cfg(test)]
+mod workspace_isolation;
+
+#[cfg(test)]
+mod tab_isolation;
+
+#[cfg(test)]
+mod profile_isolation;
+
+#[cfg(test)]
+mod disposable_session;
+
+#[cfg(test)]
+mod fail_closed;
+
+#[cfg(test)]
+mod engine_hot_swap;
+
+#[cfg(test)]
+mod network_failover;
+
+#[cfg(test)]
+mod dns_failover;
+
+#[cfg(test)]
+mod escape_response;
+
+#[cfg(test)]
+mod full_privacy_profile;
+
+#[cfg(test)]
+mod maximum_privacy_profile;
+
+#[cfg(test)]
+mod end_to_end_security;
